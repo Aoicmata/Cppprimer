feb30f92ba10a3f20799c576c5cf877da0168823
plasmadstu <plasmadstu@outlook.com>
2022-07-16 12:44:34 +0800

    1.15注释

--- 1.4.1/1.11/1.11.cpp
+++ 1.4.1/1.11/1.11.cpp
@@ -3,15 +3,15 @@ int main()
 {
         int sum = 0,val = 0;
         std::cin >> sum >> val;
-        while (sum >= val)
+        while (sum >= val)//  前数大于后数
         {
                 std::cout << sum << " ";
-                sum -= 1;
+                sum -= 1;//递减
         }
-        while (sum <= val)
+        while (sum <= val)//  前数小于后数
         {
                 std::cout << sum << " ";
-                sum += 1;
+                sum += 1;//递加
         }
         std::cout << std::endl;
         return 0;
